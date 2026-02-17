d53e6117140a4b2ee194c5078f1525f506ddc4ff
Madhusudan440 <madhusudankm50@gmail.com>
2026-02-17 22:14:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,6 @@ def init_db():
     cur.close()
     conn.close()
 
-init_db()
 
 # ==========================================
 # DISABLE BACK BUTTON CACHE
@@ -270,6 +269,8 @@ def logout():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
+
 
 
 
