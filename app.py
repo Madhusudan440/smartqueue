bf8c18a35dcfd9e1ee0c17b4a4df1ddee14d11ef
Madhusudan440 <madhusudankm50@gmail.com>
2026-02-17 21:45:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def get_db_connection():
 # AUTO CREATE TABLE
 # ==========================================
 
-def :
+def init_db():
     conn = get_db_connection()
     cur = conn.cursor()
 
@@ -270,6 +270,7 @@ def logout():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
